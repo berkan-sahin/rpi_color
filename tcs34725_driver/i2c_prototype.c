090b8eee8ea4ac19e04fbc2a00201fb7b57abe86
Berkan Şahin <tt.berkan092@gmail.com>
2020-05-09 21:49:57 +0300

    Normalleştirme algoritması eklendi

--- tcs34725_driver/i2c_prototype.c
+++ tcs34725_driver/i2c_prototype.c
@@ -9,17 +9,21 @@
 int main (void)
 {
 	int fd;
-	uint8_t id; 
-	uint16_t r, g, b, c;
-	struct timespec integration_time; /* Gerekli bekleme süresi */
+	uint8_t id, red, grn, blu; 		/* normalleştirilmiş RGB değerleri*/ 
+	uint16_t r_raw, g_raw, b_raw, c_raw; 	/* sensörden gelen saf değerler */
+	uint32_t toplam; 			/* normalleştirme algoritması için gerekli */
+
+	struct timespec integration_time; 	/* Gerekli bekleme süresi */
 	integration_time.tv_sec = 0;
-	integration_time.tv_nsec = INT_TIME; /* 101 ms */
+	integration_time.tv_nsec = INT_TIME; 	/* 101 ms */
+
 	fd = wiringPiI2CSetup (SLAVE_ID);
 	if (fd == -1)
 	{
 		printf ("wiringPiI2CSetup hata, errno: %d\n", errno);
 		return (-1);
 	}
+
 	id = wiringPiI2CReadReg8 (fd, REG_ID); /* baglanti kontrolu */
 	if (id != ID_VAL)
 	{
@@ -34,11 +38,21 @@ int main (void)
 	for(;;)
 	{
 		nanosleep(&integration_time, NULL);
-		c = (uint16_t) (wiringPiI2CReadReg16 (fd, REG_CDATAL));
-		r = (uint16_t) (wiringPiI2CReadReg16 (fd, REG_RDATAL));
-		g = (uint16_t) (wiringPiI2CReadReg16 (fd, REG_GDATAL));
-		b = (uint16_t) (wiringPiI2CReadReg16 (fd, REG_BDATAL));
-		printf	("Clear: %x 	RGB:#%x%x%x\n", c, r, g, b);
+		c_raw = (uint16_t) (wiringPiI2CReadReg16 (fd, REG_CDATAL));
+		r_raw = (uint16_t) (wiringPiI2CReadReg16 (fd, REG_RDATAL));
+		g_raw = (uint16_t) (wiringPiI2CReadReg16 (fd, REG_GDATAL));
+		b_raw = (uint16_t) (wiringPiI2CReadReg16 (fd, REG_BDATAL));
+		
+		/* Normalleştirme algoritması */
+		toplam = c_raw;
+		if (toplam == 0)
+			red = grn = blu = 0;	/* Eğer clear 0 ise siyah renk döndür */
+		
+		red = (uint8_t) ((float)r_raw / toplam * 255.0);
+		grn = (uint8_t) ((float)g_raw / toplam * 255.0);
+		blu = (uint8_t) ((float)b_raw / toplam * 255.0);
+
+		printf ("Renk: #%02X%02X%02X\n", red, grn, blu);
 	}
 	return (0);
 }
