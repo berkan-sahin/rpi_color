9c4442f361d7a9ea94022bea8d4f23ecd8b6e845
berkan <tt.berkan092@gmail.com>
2020-06-02 21:22:04 +0300

    değişken ismi düzeltildi

--- tcs34725_driver/tcs34725.c
+++ tcs34725_driver/tcs34725.c
@@ -37,16 +37,16 @@ void tcs34725_get_color(int fd, uint8_t *r, uint8_t *g, uint8_t *b)
     uint16_t c_raw, r_raw, g_raw, b_raw;
     uint32_t toplam;
     c_raw = (uint16_t) (wiringPiI2CReadReg16(fd, REG_CDATAL));
-	r_raw = (uint16_t) (wiringPiI2CReadReg16(fd, REG_RDATAL));
-	g_raw = (uint16_t) (wiringPiI2CReadReg16(fd, REG_GDATAL));
-	b_raw = (uint16_t) (wiringPiI2CReadReg16(fd, REG_BDATAL));
+    r_raw = (uint16_t) (wiringPiI2CReadReg16(fd, REG_RDATAL));
+    g_raw = (uint16_t) (wiringPiI2CReadReg16(fd, REG_GDATAL));
+    b_raw = (uint16_t) (wiringPiI2CReadReg16(fd, REG_BDATAL));
 
-	/* Normalleştirme algoritması */
-	toplam = c_raw;
-	if (toplam == 0)
-		red = grn = blu = 0;	/* Eğer clear 0 ise siyah renk döndür */
+    /* Normalleştirme algoritması */
+    toplam = c_raw;
+    if (toplam == 0)
+        *r = *g = *b = 0;    /* Eğer clear 0 ise siyah renk döndür */
 
-	*r = (uint8_t) ((float)r_raw / toplam * 255.0);
-	*g = (uint8_t) ((float)g_raw / toplam * 255.0);
-	*b = (uint8_t) ((float)b_raw / toplam * 255.0);
+    *r = (uint8_t) ((float)r_raw / toplam * 255.0);
+    *g = (uint8_t) ((float)g_raw / toplam * 255.0);
+    *b = (uint8_t) ((float)b_raw / toplam * 255.0);
 }
